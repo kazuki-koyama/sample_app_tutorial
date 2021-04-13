8ed67671cc4bb912cb871972f995011b4f70b24e
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-04-13 05:26:46 +0000

    [Add] 送信メールのテスト、user#createアクションの更新

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: '0e4a421de6e741c9941d6eb72094b409.vfs.cloud9.ap-northeast-1.amazonaws.com' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
