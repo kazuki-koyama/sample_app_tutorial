8e50379a35c1df934b3026cb75a383f0d9105a78
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-04-13 04:20:00 +0000

    [Add] AccountActivationsリソース

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,15 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      email.downcase!
+    end
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
