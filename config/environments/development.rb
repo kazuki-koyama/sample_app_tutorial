db1fd4b389084d8abe19208d5a0d8d8a01c92edc
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-04-13 05:10:44 +0000

    [Add] Action Mailerの送信メールプレビュー

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = '0e4a421de6e741c9941d6eb72094b409.vfs.cloud9.ap-northeast-1.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
