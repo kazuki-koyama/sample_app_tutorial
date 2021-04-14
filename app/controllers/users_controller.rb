8fde4d9a315b8ddfcc7a62c9040e81d838c3ddce
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-04-14 22:06:18 +0000

    [Add] Following/Followersページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -28,7 +28,6 @@ class UsersController < ApplicationController
   def edit
     user = User.find(params[:id])
   end
-
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -43,6 +42,19 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
+  end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
